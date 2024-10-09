e898ee959826d2546a3b479eaa8b875a57a96238
Jack Uterhark <jackuterhark@gmail.com>
2024-10-09 10:30:23 +0100

    looking good

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
       colors: {
         'dark-blue': '#192841',
       },
+      fontSize: {
+        '10xl': ['10rem', { lineHeight: '0.8' }], // Adjust font size and line-height
+      },
     },
   },
   plugins: [],
