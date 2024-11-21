1c657fe9da4f3b9468efeba5666d7a779c25f1ae
Jack Uterhark <jackuterhark@gmail.com>
2024-11-21 15:41:18 +0000

    contact form added

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,14 @@ module.exports = {
     extend: {
       colors: {
         'dark-blue': '#192841',
+        'hover-dark-blue': '#0e1725',
+        'focus-dark-blue': '030508',
       },
       fontSize: {
         '10xl': ['10rem', { lineHeight: '0.8' }], // Adjust font size and line-height
+      },
+      boxShadow: {
+        '3xl': '1px 2px 2px 1px',
       },
     },
   },
